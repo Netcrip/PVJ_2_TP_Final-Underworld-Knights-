525e942dccc47fab57dc84935c05a639018de7f0
Juan Pastor <62908450+Zearinox@users.noreply.github.com>
2024-11-16 18:30:47 -0300

    entorno, trampas y paredes
    Se quitaron colisiones de objetos decorativos del recorrido.
    Se incluyo daño al jugador al script de trampa.
    Se modificaron prefabs de path y paredes para desactivarse al ser empujado por las trampas y volver a activarse al tocar el suelo

--- Assets/Scripts/Traps/TrapDamagePush.cs
+++ Assets/Scripts/Traps/TrapDamagePush.cs
@@ -6,13 +6,25 @@ using UnityEngine;
 public class Trap : MonoBehaviour
 {
     private CharacterController _characterController;
+    public float damage = 10f; // Daño por golpe al jugador
     private float pushForce = 50f; // Fuerza inicial del empuje
     private float pushDecay = 5f; // Velocidad a la que el empuje disminuye
     private Vector3 _pushDirection; // Dirección del empuje 
-    
+
+    public Collider[] invisibleWalls; // Lista de colisionadores de las paredes invisibles
+
     private void Start()
     {
         _characterController = GameObject.FindWithTag("Player").GetComponent<CharacterController>();
+
+        // Encuentra todas las paredes invisibles en la escena por su etiqueta
+        GameObject[] walls = GameObject.FindGameObjectsWithTag("InvisibleWall");
+        invisibleWalls = new Collider[walls.Length];
+
+        for (int i = 0; i < walls.Length; i++)
+        {
+            invisibleWalls[i] = walls[i].GetComponent<Collider>();
+        }
     }
 
     private void Update()
@@ -23,7 +35,6 @@ public class Trap : MonoBehaviour
             // Mueve el CharacterController en la dirección del empuje
             _characterController.Move(_pushDirection * Time.deltaTime);
     
-
             // Reduce gradualmente el empuje hasta que se detenga
             _pushDirection = Vector3.Lerp(_pushDirection, Vector3.zero, pushDecay * Time.deltaTime);
         }
@@ -36,12 +47,34 @@ public class Trap : MonoBehaviour
         _pushDirection = -direction.normalized * pushForce;
     }
     private void OnCollisionEnter(Collision collision)
-{
-    // Obtén el primer punto de contacto
-    Vector3 impactPoint = collision.contacts[0].point;
+    {
+        // Comprueba si el objeto con el que colisionó tiene un componente PlayerHealth
+        PlayerHealth playerHealth = collision.collider.GetComponent<PlayerHealth>();
 
-    // Llama a ApplyPush para aplicar el empuje
-    ApplyPush(impactPoint);
-}
+        // Obtén el primer punto de contacto
+        Vector3 impactPoint = collision.contacts[0].point;
 
+        // Llama a ApplyPush para aplicar el empuje
+        ApplyPush(impactPoint);
+
+        // Llamado al metodo que aplica el daño
+        ApllyDamage(playerHealth);
+
+        // Desactiva las paredes invisibles
+        ToggleInvisibleWalls(false);
+    }
+
+    public void ApllyDamage(PlayerHealth playerHealth)
+    {
+        if (playerHealth != null)        
+            playerHealth.Damage(damage);        
+    }
+
+    public void ToggleInvisibleWalls(bool isActive)
+    {
+        foreach (var wall in invisibleWalls)
+        {
+            wall.enabled = isActive;
+        }
+    }
 }
